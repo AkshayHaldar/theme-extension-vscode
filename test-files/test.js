cb7836a034c75068649a4ec94e957d6349157b9f
About me <akshayhaldar.dev@gmail.com>
2025-05-12 19:02:04 +0530

    first commit

---
+++ test-files/test.js
@@ -0,0 +1,8 @@
+function helloWorld() {
+    console.log("Testing WebDev theme");
+    return true;
+}
+
+if (require.main === module) {
+    helloWorld();
+} 
---
+++ test-files/test.py
@@ -0,0 +1,6 @@
+def hello_world():
+    print("Testing Python theme")
+    return True
+
+if __name__ == "__main__":
+    hello_world() 
